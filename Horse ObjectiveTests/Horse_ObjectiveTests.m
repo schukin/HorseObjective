2f3d37c1648a6902f1d5ae88456112fb1d3837f5
Dave Schukin <dave@cotap.com>
2013-06-29 13:51:56 -0700

    Initial Commit

---
+++ Horse ObjectiveTests/Horse_ObjectiveTests.m
@@ -0,0 +1,36 @@
+//
+//  Horse_ObjectiveTests.m
+//  Horse ObjectiveTests
+//
+//  Created by Dave Schukin on 6/29/13.
+//  Copyright (c) 2013 Dave Schukin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Horse_ObjectiveTests : XCTestCase
+
+@end
+
+@implementation Horse_ObjectiveTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
