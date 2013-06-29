98bf75d8d89b60ff3eeb48f572481f90e0f086d2
Dave Schukin <dave@cotap.com>
2013-06-29 14:04:27 -0700

    Apple announced its financial results for the assistant editor in Interface Builder

---
+++ 🐴Header.h
@@ -0,0 +1,17 @@
+//
+//  🐴Header.h
+//  Horse Objective
+//
+
+#ifndef Horse_Objective___Header_h
+#define Horse_Objective___Header_h
+
+#define __unsafe_unretained_uncensored __strong
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > 70000
+    #define __readmaybe __readwrite
+#else
+    #define __readmaybe __readonly
+#endif
+
+#endif
