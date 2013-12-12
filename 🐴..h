63e5ebf303564a7f99397ea6e8e540da63f570ea
Dave Schukin <dave@clinkle.com>
2013-12-11 23:24:25 -0800

    Overriding drawOval: may negatively affect Scott Forstall

--- 🐴..h
+++ 🐴..h
@@ -6,12 +6,32 @@
 #ifndef Horse_Objective___Header_h
 #define Horse_Objective___Header_h
 
+/**
+  Let people know we're serious.
+  */
 #define __unsafe_unretained_uncensored __strong
 
-#if __IPHONE_OS_VERSION_MAX_ALLOWED > 70000
+/**
+  Things are all buttersmooth today?
+  Introduce some fun into your project by
+  blowing up in the future.
+  */
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > 80000
     #define __readmaybe __readwrite
 #else
     #define __readmaybe __readonly
 #endif
+
+/**
+  Just switched to ARC? Good news! You can
+  still do all the performSelectoring your heart desires!
+  */
+#define LOLSuppressPerformSelectorLeakWarning(Stuff) \
+    do { \
+        _Pragma("clang diagnostic push") \
+        _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
+        Stuff; \
+        _Pragma("clang diagnostic pop") \
+    } while (0)
 
 #endif
